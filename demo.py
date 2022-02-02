fd66355fac3cbec3a6558991d1af6c5b2a6ef7e7
bairuic <brc730.outlook.com>
2022-02-02 18:56:59 +0800

    update demo

--- demo.py
+++ demo.py
@@ -1,20 +1,37 @@
 from net.linear import Linear
-from net.activation import ReLU
+from net.activation import Sigmoid
 from net.sequential import Sequential
 from net.loss import MSE
+from optim.SGD import SGD
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 inputs = np.arange(100).reshape(100,1)
 targets = np.sin(inputs)
 
-model = Sequential(
+mod = Sequential(
     Linear(1, 8),
-    ReLU(),
+    Sigmoid(),
     Linear(8, 1),
 )
 
 criterion = MSE()
-optim = SGD()
+optim = SGD(model=mod)
+
+loss_arr = []
+for i in range(1000):
+    optim.zero_grad()
+    outputs = mod(inputs)
+    loss = criterion(outputs, targets)
+    loss_grad = criterion.backward()
+    mod.backward(loss_grad)
+    optim.step()
+    loss_arr.append(loss)
+    if i % 100 == 0:
+        print(loss)
+
+plt.plot(loss_arr)
+plt.show()
+
 
 
