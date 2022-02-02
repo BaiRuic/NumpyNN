28dc7cf475ef94b737026860c51bc174bea315dd
bairuic <brc730.outlook.com>
2022-02-02 18:51:04 +0800

    update sequence

--- net/sequential.py
+++ net/sequential.py
@@ -19,7 +19,7 @@ class Sequential(Module):
         return input_
 
     def backward(self, pre_grad, *args, **kwargs):
-        for layer in self.layers:
+        for layer in self.layers[::-1]:
             pre_grad = layer.backward(pre_grad)
 
     @property
