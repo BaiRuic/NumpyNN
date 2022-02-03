5dacb520ed7000ec7fc87d0861cd673d61a5fbb0
bairuic <brc730.outlook.com>
2022-02-03 17:28:37 +0800

    update sgd

--- optim/SGD.py
+++ optim/SGD.py
@@ -1,12 +1,13 @@
 from .optimizer import Optimizer
 
 class SGD(Optimizer):
+    """
+    随机梯度下降
+    """
     def __init__(self, momentum=0.9, *args, **kwargs):
         super(SGD, self).__init__(*args, **kwargs)
         self.momentum = momentum
 
     def step(self):
         for params, grads in zip(self.model.params, self.model.grads):
-            # print("p:", params)
-            # print("g:", grads)
             params -= self.lr * grads
